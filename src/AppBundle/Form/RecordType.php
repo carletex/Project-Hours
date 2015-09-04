ca062837535439116d9cc9340ffc9c6d6266e309
carletex <oceanrdn@gmail.com>
2015-09-04 17:14:59 +0200

    Order in selection

--- src/AppBundle/Form/RecordType.php
+++ src/AppBundle/Form/RecordType.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Form;
 
+use Doctrine\ORM\EntityRepository;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
@@ -22,11 +23,14 @@ class RecordType extends AbstractType{
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
-
         $builder
             ->add('project', 'entity', array(
               'class' => 'AppBundle:Project',
+              'query_builder' => function (EntityRepository $er) {
+                return $er->createQueryBuilder('p')
+                  ->innerJoin('p.client','c')
+                  ->orderBy('c.name', 'ASC');
+              },
               'group_by' => 'client',
               'placeholder' => ''
             ))
